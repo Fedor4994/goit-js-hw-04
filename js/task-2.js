9ca2bff6e16f904c40340a73d79c97ae2dde837a
Fedor4994 <fedorsosnin04@gmail.com>
2022-08-19 16:32:54 +0300

    Делает дз4

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    this.items.push(itemName);
+    console.log(`Adding ${itemName} to inventory`);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+    console.log(`Removing ${itemName} from inventory`);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  return action;
+};
+
+invokeInventoryAction('Medkit', inventory.add('Medikit'));
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove('Gas mask'));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
